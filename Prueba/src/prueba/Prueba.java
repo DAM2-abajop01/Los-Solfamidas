63e3752e825cb49975f6827b9669633cdd0faaaa
IsmA <IsmA@deLimaMcfly>
2016-11-23 20:13:48 +0100

    subo el metodo Ismael

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -10,8 +10,12 @@ public class Prueba {
         System.out.println("Hola Mundo !!!!");
         System.out.println("EEEEEE");
     }
-    
-    public static void Marcos(){
+
+    public static void Marcos() {
         System.out.println("Hola");
+    }
+
+    public static void Ismael() {
+        System.out.println("weeee");
     }
 }
